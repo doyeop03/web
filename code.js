2b107acccad181c0ea8fbfc70136caa6e4230959
doyeop03 <doyeop0130@gmail.com>
2022-10-28 14:02:20 +0900

    make sign up page

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
 function join(){
     window.location.href=
-    'https://www.naver.com/'
+    'file:///C:/Users/doyeo/OneDrive/%EB%AC%B8%EC%84%9C/web/signupPage/signup.html'
 }
---
+++ signupPage/signup.js
@@ -0,0 +1,4 @@
+function join(){
+    window.location.href=
+    'file:///C:/Users/doyeo/OneDrive/%EB%AC%B8%EC%84%9C/web/webprogrammingtest.html'
+}
